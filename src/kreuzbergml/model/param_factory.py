9bec4f08fc2d5b614c6fd7b22d3aa1e6ab6c6738
Samuel Brinkmann <samuel@brinkmann-familie.de>
2022-05-20 14:25:48 +0200

    added RFC ParamFactory
    - added solver = liblinear to l2 penalty LR

--- src/kreuzbergml/model/param_factory.py
+++ src/kreuzbergml/model/param_factory.py
@@ -22,7 +22,7 @@ class L2RegularizedLRParamsFactory(AbstractGridSearchParamsFactory):
     def get_param_dict(self) -> Dict[str, Iterable]:
         param_dict = {
             "penalty": ["l2"],
-            "solver": ["lbfgs"],
+            "solver": ["lbfgs", "liblinear"],
             "C": list(range(11)),
         }
         return param_dict
@@ -38,5 +38,20 @@ class ENRegularizedLRParamsFactory(AbstractGridSearchParamsFactory):
             "solver": ["saga"],
             "C": list(range(11)),
             "l1_ratio": list(np.linspace(0, 1, num=5)),
+        }
+        return param_dict
+
+class RFCParamsFactory(AbstractGridSearchParamsFactory):
+    def get_model_class(self):
+        return sklearn.ensemble.RandomForestClassifier
+
+    def get_param_dict(self) -> Dict[str, Iterable]:
+        param_dict = {
+            "max_depth": list(range(2,11)),
+            "n_estimator": [8, 16, 32, 64, 100, 200, 500, 1000],
+            "min_samples_split": [2, 3, 5, 10],
+            "min_samples_leaf": [1, 2, 4],
+            "bootstrap": [True, False],
+            "criterion": ["gini", "entropy"],
         }
         return param_dict
