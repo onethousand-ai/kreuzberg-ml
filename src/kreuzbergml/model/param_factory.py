ae00c47e41ccf559bb184aeefcaa4026c99f2aa8
Samuel Brinkmann <samuel@brinkmann-familie.de>
2022-05-20 14:39:07 +0200

    added MLPClassifier

--- src/kreuzbergml/model/param_factory.py
+++ src/kreuzbergml/model/param_factory.py
@@ -47,7 +47,7 @@ class RFCParamsFactory(AbstractGridSearchParamsFactory):
 
     def get_param_dict(self) -> Dict[str, Iterable]:
         param_dict = {
-            "max_depth": list(range(2,11)),
+            "max_depth": list(range(4,10)),
             "n_estimator": [8, 16, 32, 64, 100, 200, 500, 1000],
             "min_samples_split": [2, 3, 5, 10],
             "min_samples_leaf": [1, 2, 4],
@@ -78,5 +78,19 @@ class LinSVCParamsFactory(AbstractGridSearchParamsFactory):
             "penalty": ["l1", "l2"],
             "dual": [True, False],
             "C": [1e-5, 1e-4, 1e-3, 1e-2, 0.1, 1, 10, 100, 1000, 10000, 100000],
+        }
+        return param_dict
+
+class MLPCParamsFactory(AbstractGridSearchParamsFactory):
+    def get_model_class(self):
+        return sklearn.neural_network.MLPClassifier
+
+    def get_param_dict(self) -> Dict[str, Iterable]:
+        param_dict = {
+            "hidden_layer_sizes": [(10,), (20,), (100,), (10, 30, 10), (50,50,50), (50,100,50)],
+            "activation": ["tanh", "relu", "logistic"],
+            "solver": ["sgd", "adam"],
+            "alpha": [0.0001, 0.001, 0.01, 0.05],
+            "learning_rate": ["constant", "adaptive"],
         }
         return param_dict
