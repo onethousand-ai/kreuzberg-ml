8b32f5fac95bf0eb3e40df837e89192532946df2
tahleen-rahman <deesharahman@gmail.com>
2022-05-30 16:30:43 +0200

    updates according to review

--- src/kreuzbergml/data_quality/DataFrameStatistics.py
+++ src/kreuzbergml/data_quality/DataFrameStatistics.py
@@ -1,43 +1,38 @@
-from abc import ABC
 from typing import List, Optional, Union
 from pandas import DataFrame, Series, Index, DatetimeIndex, PeriodIndex, TimedeltaIndex
-from enum import Enum
-import logging
+from enum import Enum, auto
 from logging import getLogger
+
+logger = getLogger(__name__)
 
 
 class DataFrameType(Enum):
     """
-    Enum of supported dataset types.
+    Supported dataframe types.
     """
-    TABULAR = 'tabular'
-    TIMESERIES = 'timeseries'
-    # TODO currently only the above 2 are supported
-    IMAGE = 'image'
-    VIDEO = 'video'
-    AUDIO = 'audio'
+    TABULAR = auto()
+    TIMESERIES = auto()
 
 
-class DataQuality(ABC):
+class DataFrameStatistics:
 
     def __init__(self,
                  df: DataFrame,
                  random_state: Optional[int] = None,
                  dtypes: dict = None,
-                 severity: Optional[str] = None):
+                 # severity: Optional[str] = None
+                 ):
         self._df = df
-        self._df_type = None
-        self._warnings = []
-        self._logger = getLogger(__name__)
-        self._dtypes = dtypes
-        self._random_state = random_state
+        # self._df_type = None
+        # self._warnings = []
+        # self._dtypes = dtypes
 
     @property
     def df(self):
         "Target of data quality checks."
         return self._df
 
-    def _count_nulls(self, col: Union[List[str], str, None] = None, perc=False):
+    def _count_nulls(self, col: Union[List[str], str, None] = None):#, perc=False):
         """
 
         :param col: column name if provided only counts null in that column
@@ -47,11 +42,9 @@ class DataQuality(ABC):
         """
 
         count = self.df.isnull().sum() if col is None else self.df[col].isnull().sum()
-        perc_ = count / len(self.df)
-        if not perc:
-            return count
-        else:
-            return count, perc_
+        # null_values_percentage = count / len(self.df)
+
+        return count #, null_values_percentage
 
     def get_null_cols(self, col: Optional[str] = None) -> List[str]:
         """
@@ -77,13 +70,18 @@ class DataQuality(ABC):
                     dupes.setdefault(col, []).append(tgt_col)  # Store if they match
         return dupes
 
-    def report(self):
+    def calc_statistics(self):
+        dupes_dict = self.get_duplicate_columns()
+        cols_with_dupes = len(dupes_dict.keys())
+        null_cols = self.get_null_cols()
+        return dupes_dict, cols_with_dupes, null_cols
+
+    def print_report(self):
         """
         Prints a report containing all the warnings detected during the data quality analysis.
         """
+        dupes_dict, cols_with_dupes, null_cols = self.calc_statistics()
 
-        dupes_dict = self.get_duplicate_columns()
-        cols_with_dupes = len(dupes_dict.keys())
         print(f"\n\nDATA QUALITY REPORT\n")
         if cols_with_dupes > 0:
             print(f"Found {cols_with_dupes} columns with exactly the same feature values as other columns.")
@@ -92,25 +90,23 @@ class DataQuality(ABC):
         else:
             print("No duplicate columns were found.")
 
-        null_cols = self.get_null_cols()
-
         if len(null_cols) > 0:
             print(f"The following columns have missing values:")
             for col in null_cols:
-                count, perc_ = self._count_nulls(col, perc=True)
-                print(f"Column '{col}' has {count} missing values which comprise {round(perc_, 2)}% of all rows")
+                count = self._count_nulls(col)
+                percentage_nulls = count / len(self.df)
+
+                print(f"Column '{col}' has {count} missing values which comprise {round(percentage_nulls, 2)}% of all rows")
         else:
             print(f"No missing values were found")
 
-    def check_time_index(index: Index) -> bool:
-        """Tries to infer from passed index column if the dataframe is a timeseries or not."""
-        if isinstance(index, (DatetimeIndex, PeriodIndex, TimedeltaIndex)):
-            return True
-        return False
-
-    def infer_df_type(self, df: DataFrame) -> DataFrameType:
-        """Simple inference method to dataset type."""
-        if self.check_time_index(df.index):
-            return DataFrameType.TIMESERIES
-        # TODO other types
-        return DataFrameType.TABULAR
+    # def check_time_index(self, index: Index) -> bool:
+    #     """Tries to infer from passed index column if the dataframe is a timeseries or not."""
+    #     if isinstance(index, (DatetimeIndex, PeriodIndex, TimedeltaIndex)):
+    #         return True
+    #     return False
+    #
+    # def infer_df_type(self, df: DataFrame) -> DataFrameType:
+    #     if self.check_time_index(df.index):
+    #         return DataFrameType.TIMESERIES
+    #     return DataFrameType.TABULAR
