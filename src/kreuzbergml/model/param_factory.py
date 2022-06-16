cffb15927ed5c8a3194c0e07adaf0713551f68dc
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2022-06-16 09:14:23 +0200

    Param factory dev (#4)
    * added RFC ParamFactory
    
    - added solver = liblinear to l2 penalty LR
    
    * added SVC and LinearSVC
    
    * added MLPClassifier
    
    * added DTC
    
    - changed some parameters from SVC
    
    * added KNC
    
    * Update param_factory.py
    
    * added missing imports for param_factory
    
    * changed Hyperparameters of RFC to reduce time

--- src/kreuzbergml/model/param_factory.py
+++ src/kreuzbergml/model/param_factory.py
@@ -2,7 +2,12 @@ from abc import ABC, abstractmethod
 from typing import Dict, Iterable
 
 import numpy as np
-import sklearn
+import sklearn.ensemble
+import sklearn.linear_model
+import sklearn.neighbors
+import sklearn.neural_network
+import sklearn.svm
+import sklearn.tree
 
 
 class AbstractGridSearchParamsFactory(ABC):
@@ -38,5 +43,81 @@ class ENRegularizedLRParamsFactory(AbstractGridSearchParamsFactory):
             "solver": ["saga"],
             "C": list(range(11)),
             "l1_ratio": list(np.linspace(0, 1, num=5)),
+        }
+        return param_dict
+
+class RFCParamsFactory(AbstractGridSearchParamsFactory):
+    def get_model_class(self):
+        return sklearn.ensemble.RandomForestClassifier
+
+    def get_param_dict(self) -> Dict[str, Iterable]:
+        param_dict = {
+            "max_depth": list(range(4,10)),
+            "n_estimators": [2**i for i in range(3, 7)],
+            "min_samples_split": [2, 3, 5, 10],
+            "min_samples_leaf": [1, 2, 4],
+        }
+        return param_dict
+
+class SVCParamsFactory(AbstractGridSearchParamsFactory):
+    def get_model_class(self):
+        return sklearn.svm.SVC
+
+    def get_param_dict(self) -> Dict[str, Iterable]:
+        param_dict = {
+            "kernel": ["rbf", "poly", "sigmoid"],
+            "gamma": np.logspace(-3, 1, num=5),
+            "C": np.logspace(-1, 2, num=4),
+        }
+        return param_dict
+
+class LinSVCParamsFactory(AbstractGridSearchParamsFactory):
+    def get_model_class(self):
+        return sklearn.svm.LinearSVC
+
+    def get_param_dict(self) -> Dict[str, Iterable]:
+        param_dict = {
+            "penalty": ["l1", "l2"],
+            "dual": [True, False],
+            "C": np.logspace(-5,5, num=11),
+        }
+        return param_dict
+
+class MLPCParamsFactory(AbstractGridSearchParamsFactory):
+    def get_model_class(self):
+        return sklearn.neural_network.MLPClassifier
+
+    def get_param_dict(self) -> Dict[str, Iterable]:
+        param_dict = {
+            "hidden_layer_sizes": [(10,), (20,), (100,), (10, 30, 10), (50,50,50), (50,100,50)],
+            "activation": ["tanh", "relu", "logistic"],
+            "alpha": np.logspace(-4,-2, num=7),
+            "learning_rate": ["constant", "adaptive"],
+        }
+        return param_dict
+
+class DTCParamsFactory(AbstractGridSearchParamsFactory):
+    def get_model_class(self):
+        return sklearn.tree.DecisionTreeClassifier
+
+    def get_param_dict(self) -> Dict[str, Iterable]:
+        param_dict = {
+            "criterion": ["gini", "entropy"],
+            "max_depth": list(range(3, 10)),
+            "min_samples_split": list(range(2, 10)),
+            "min_samples_leaf": list(range(1, 5)),
+        }
+        return param_dict
+
+class KNCParamsFactory(AbstractGridSearchParamsFactory):
+    def get_model_class(self):
+        return sklearn.neighbors.KNeighborsClassifier
+
+    def get_param_dict(self) -> Dict[str, Iterable]:
+        param_dict = {
+            "n_neighbors": list(range(1, 30, 3)),
+            "p": list(range(1,5)),
+            "leaf_size": list(range(1, 50, 5)),
+            "weights": ["uniform", "distance"],
         }
         return param_dict
