935c16cb6657389b8ece36e8bdf86be26fde3982
iyalchyk <ilya.yalchyk@lead.berlin>
2022-06-20 08:58:11 +0200

    added create_pipeline_endpoint and database tools

--- src/kreuzbergml/deployment/azure/app.py
+++ src/kreuzbergml/deployment/azure/app.py
@@ -1,38 +1,61 @@
 import logging
+from typing import Dict, Optional
 
 import gin
-from azureml.core import ComputeTarget, Workspace, Model, Environment, Webservice
-from azureml.core.authentication import InteractiveLoginAuthentication
+from azureml.core import ComputeTarget, Environment, Model, Webservice, Workspace
+from azureml.core.authentication import (
+    InteractiveLoginAuthentication,
+    ServicePrincipalAuthentication,
+)
 from azureml.core.compute import AmlCompute
 from azureml.core.model import InferenceConfig
 from azureml.core.webservice import AciWebservice
 from azureml.exceptions import ComputeTargetException
+from azureml.core import Model, RunConfiguration
+from azureml.core.webservice import AciWebservice
+from azureml.pipeline.core import Pipeline, PipelineEndpoint
+from azureml.pipeline.steps import PythonScriptStep
 
 logger = logging.getLogger(__name__)
 
 
 @gin.configurable
 class AzureApp:
+    instance: Optional[object] = None
+
     def __init__(
-            self,
-            code_path: str,
-            tenant_id: str,
-            subscription_id: str,
-            resource_group: str,
-            workspace_name: str,
-            compute_name: str,
-            realtime_service_name: str,
-            batch_service_name: str,
-            app_name: str = "app",
+        self,
+        # code_path: str,
+        tenant_id: str,
+        subscription_id: str,
+        service_principle_id: str,
+        service_principle_password: str,
+        resource_group: str,
+        workspace_name: str,
+        # model_name: str,
+        # code_path: str,
+        compute_name: str,
+        # realtime_service_name: str,
+        # batch_service_name: str,
+        # app_name: str = "app",
     ):
-        # self.__code_path = code_path
         self.__tenant_id = tenant_id
         self.__subscription_id = subscription_id
+        self.__service_principle_id = service_principle_id
+        self.__service_principle_password = service_principle_password
         self.__resource_group = resource_group
         self.__workspace_name = workspace_name
+        # self.__model_name = model_name
+        # self.__code_path = code_path
         self.__compute_name = compute_name
-        self.__realtime_service_name = realtime_service_name
-        self.__batch_service_name = batch_service_name
+        # self.__realtime_service_name = realtime_service_name
+        # self.__batch_service_name = batch_service_name
+
+    @staticmethod
+    def get_instance():
+        if not AzureApp.instance:
+            AzureApp.instance = AzureApp()
+        return AzureApp.instance
 
     @property
     def tenant_id(self) -> str:
@@ -40,53 +63,81 @@ class AzureApp:
 
     @property
     def subscription_id(self) -> str:
-        return self.__tenant_id
+        return self.__subscription_id
+
+    @property
+    def service_principle_id(self) -> str:
+        return self.__service_principle_id
+
+    @property
+    def service_principle_password(self) -> str:
+        return self.__service_principle_password
 
     @property
     def resource_group(self):
-        return self.__tenant_id
+        return self.__resource_group
 
     @property
     def workspace_name(self) -> str:
-        return self.__tenant_id
+        return self.__workspace_name
+
+    # @property
+    # def model_name(self) -> str:
+    #     return self.__model_name
+    #
+    # @property
+    # def code_path(self) -> str:
+    #     return self.__code_path
 
     @property
     def compute_name(self) -> str:
         return self.__compute_name
 
-    @property
-    def realtime_service_name(self) -> str:
-        return self.__realtime_service_name
-
-    @property
-    def batch_service_name(self) -> str:
-        return self.__batch_service_name
+    # @property
+    # def realtime_service_name(self) -> str:
+    #     return self.__realtime_service_name
+    #
+    # @property
+    # def batch_service_name(self) -> str:
+    #     return self.__batch_service_name
 
     def get_workspace(self) -> Workspace:
-        auth = self.get_interactive_authentication()
+        if self.service_principle_id and self.service_principle_password:
+            auth = self.get_service_principle_authentication()
+        else:
+            auth = self.get_interactive_authentication()
         ws = Workspace(
             subscription_id=self.subscription_id,
             resource_group=self.resource_group,
             workspace_name=self.workspace_name,
-            auth=auth
+            auth=auth,
         )
         return ws
 
     @staticmethod
     def get_conda_env(
-            name: str = "conda-env",
-            file_path: str = "conda.yml",
+        name: str = "conda-env",
+        file_path: str = "conda.yml",
     ) -> Environment:
         return Environment.from_conda_specification(name=name, file_path=file_path)
 
     def get_interactive_authentication(self) -> InteractiveLoginAuthentication:
-        return InteractiveLoginAuthentication(tenant_id=self.tenant_id)
+        auth = InteractiveLoginAuthentication(tenant_id=self.tenant_id)
+        return auth
+
+    def get_service_principle_authentication(self) -> ServicePrincipalAuthentication:
+        auth = ServicePrincipalAuthentication(
+            tenant_id=self.tenant_id,
+            service_principal_id=self.service_principle_id,
+            service_principal_password=self.service_principle_password,
+        )
+        return auth
 
     def get_or_create_compute_target(
-            self,
-            vm_size: str = 'STANDARD_DS11_V2',
-            vm_priority: str = 'lowpriority',
-            max_nodes: int = 1,
+        self,
+        vm_size: str = "STANDARD_DS11_V2",
+        vm_priority: str = "lowpriority",
+        max_nodes: int = 1,
     ) -> ComputeTarget:
         """
         Creates a compute cluster.
@@ -129,10 +180,10 @@ class AzureApp:
         logger.info(f"Compute instance '{self.compute_name}' has been deleted.")
         return True
 
-    def register_model(
-            self,
-            model_name: str = "default_model",
-            model_path: str = "project_dir",
+    def deploy_model(
+        self,
+        model_name: str,
+        model_path: str,
     ) -> Model:
         ws = self.get_workspace()
         model = Model.register(
@@ -143,18 +194,21 @@ class AzureApp:
         return model
 
     def create_real_time_endpoint(
-            self,
-            source_dir: str,
-            entry_script_file: str = "score.py",
-            conda_file: str = "./conda.yml",
-            aci_cpu_cores: int = 1,
-            aci_memory_gb: int = 4,
+        self,
+        source_dir: str,
+        entry_script_file: str = "score.py",
+        endpoint_azure_name: str = "default-service",
+        endpoint_dns_name: Optional[str] = None,
+        conda_file: str = "./conda.yml",
+        aci_cpu_cores: int = 1,
+        aci_memory_gb: int = 0.5,
+        environment_variables: Optional[Dict[str, str]] = None,
     ) -> Webservice:
         ws = self.get_workspace()
         env = Environment.from_conda_specification(
-            name=f"{self.realtime_service_name}-env",
-            file_path=conda_file
+            name=f"{self.realtime_service_name}-env", file_path=conda_file
         )
+        env.environment_variables = environment_variables
         inference_config = InferenceConfig(
             environment=env,
             entry_script=entry_script_file,
@@ -163,10 +217,11 @@ class AzureApp:
         deployment_config = AciWebservice.deploy_configuration(
             cpu_cores=aci_cpu_cores,
             memory_gb=aci_memory_gb,
+            dns_name_label=endpoint_dns_name,
         )
         service = Model.deploy(
             workspace=ws,
-            name=self.realtime_service_name,
+            name=endpoint_azure_name,
             models=[],
             inference_config=inference_config,
             deployment_config=deployment_config,
@@ -175,3 +230,58 @@ class AzureApp:
         )
         service.wait_for_deployment(show_output=True)
         return service
+
+    def create_pipeline_endpoint(self,
+                model_name: str,
+                pipeline_endpoint_name: str,
+                                 conda_file: str = "./conda.yml",
+                environment_variables: Optional[Dict[str, str]] = None,
+        ) -> None:
+            ws = self.get_workspace()
+            env = Environment.from_conda_specification(
+                name=f"{self.realtime_service_name}-env", file_path=conda_file
+            )
+            env.environment_variables = environment_variables
+            compute_target = self.create_compute_instance()
+            runconfig = RunConfiguration()
+            runconfig.environment = env
+
+            batch_execution_step = PythonScriptStep(
+                source_directory="restocking",
+                script_name="deployment/azure_batch_forecasting.py",
+                arguments=["--model_name", model_name],
+                runconfig=runconfig,
+                compute_target=compute_target,
+                allow_reuse=False,
+            )
+            published_pipeline = Pipeline(
+                workspace=ws,
+                steps=[batch_execution_step],
+                default_source_directory="restocking",
+                description="Batch pipeline",
+            ).publish(
+                name="batch-pipeline",
+                description="Published batch pipeline.",
+            )
+            existing_pipelines_list = PipelineEndpoint.list(ws, active_only=False)
+            existing_pipelines_names_set = {p.name for p in existing_pipelines_list}
+            if pipeline_endpoint_name in existing_pipelines_names_set:
+                pipeline_endpoint = PipelineEndpoint.get(
+                    workspace=ws, name=pipeline_endpoint_name
+                )
+                pipeline_endpoint.add_default(published_pipeline)
+                if pipeline_endpoint.status == "Disabled":
+                    pipeline_endpoint.enable()
+                logging.info(
+                    f"Existing pipeline has been updated. URL={pipeline_endpoint.endpoint}"
+                )
+            else:
+                pipeline_endpoint = PipelineEndpoint.publish(
+                    workspace=ws,
+                    name=pipeline_endpoint_name,
+                    pipeline=published_pipeline,
+                    description="Batch Pipeline Endpoint",
+                )
+                logger.info(
+                    f"New pipeline has been published. URL={pipeline_endpoint.endpoint}"
+                )
